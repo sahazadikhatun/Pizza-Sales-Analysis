97948ff738a6176691f6dd18b037f40ffb847d6c
sahazadikhatun <sahazadikhatun2002@gmail.com>
2025-10-03 11:41:12 +0530

    Update 00_init_databse.sql

--- Scripts/00_init_databse.sql
+++ Scripts/00_init_databse.sql
@@ -12,6 +12,50 @@ WARNING:
     and ensure you have proper backups before running this script.
 */
 -- Create Database "MuSQL_Project_Pizza_Sales_Analysis"
-DROP DATABASE IF EXISTS pizza_sales;
-create database pizza_sales;
-use pizza_sales;
+DROP DATABASE IF EXISTS  Database Pizza_Sales_DB_1;
+Create Database Pizza_Sales_DB_1;
+use Pizza_Sales_DB_1;
+
+-- ===============================
+-- DROP TABLES IF THEY ALREADY EXIST
+-- ===============================
+DROP TABLE IF EXISTS order_details;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS pizzas;
+DROP TABLE IF EXISTS pizza_type;
+
+-- ===============================
+-- CREATE TABLES
+-- ===============================
+
+-- ORDERS TABLE
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    date DATE NOT NULL,
+    time TIME NOT NULL
+);
+
+-- ORDER DETAILS TABLE
+CREATE TABLE order_details (
+    order_details_id INT PRIMARY KEY,
+    order_id INT NOT NULL,
+    pizza_id VARCHAR(50) NOT NULL,
+    quantity INT NOT NULL
+);
+
+-- PIZZA TYPE TABLE
+CREATE TABLE pizza_type (
+    pizza_type_id VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    ingredients TEXT NOT NULL
+);
+
+-- PIZZAS TABLE
+CREATE TABLE pizzas (
+    pizza_id VARCHAR(50) PRIMARY KEY,
+    pizza_type_id VARCHAR(50) NOT NULL,
+    size VARCHAR(5) NOT NULL,
+    price DECIMAL(5,2) NOT NULL
+);
+
